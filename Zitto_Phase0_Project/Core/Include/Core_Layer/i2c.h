63f2e904621754db1c92ad58f84d2e63044318ce
EmbeddedSoftwareZitto <zittobms@gmail.com>
2024-01-09 20:55:49 +0530

    Added separate I2C Communication File
    Added separate I2C Communication header file according to the architecture

---
+++ Zitto_Phase0_Project/Core/Include/Core_Layer/i2c.h
@@ -0,0 +1,13 @@
+/*
+ * i2c.h
+ *
+ *  Created on: Jan 7, 2024
+ *      Author: Pavithraa
+ */
+
+#ifndef INCLUDE_CORE_LAYER_I2C_H_
+#define INCLUDE_CORE_LAYER_I2C_H_
+
+
+
+#endif /* INCLUDE_CORE_LAYER_I2C_H_ */
