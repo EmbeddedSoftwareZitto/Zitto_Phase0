49e3d19c0fadc11cfaf93166751d5942aadd847f
EmbeddedSoftwareZitto <zittobms@gmail.com>
2024-01-09 20:54:45 +0530

    Added separate GPIO Communication File
    Added separate GPIO Communication header file according to the architecture

---
+++ Zitto_Phase0_Project/Core/Include/Core_Layer/gpio.h
@@ -0,0 +1,13 @@
+/*
+ * gpio.h
+ *
+ *  Created on: Jan 7, 2024
+ *      Author: Pavithraa
+ */
+
+#ifndef INCLUDE_CORE_LAYER_GPIO_H_
+#define INCLUDE_CORE_LAYER_GPIO_H_
+
+
+
+#endif /* INCLUDE_CORE_LAYER_GPIO_H_ */
