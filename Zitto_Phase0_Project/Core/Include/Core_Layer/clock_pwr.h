a64b82f3ac4d6267aac7e20b2c76116d20a863e7
EmbeddedSoftwareZitto <zittobms@gmail.com>
2024-01-09 20:50:31 +0530

    Added separate Clock Source Code File
    Added separate Clock Source Code file according to the architecture

---
+++ Zitto_Phase0_Project/Core/Include/Core_Layer/clock_pwr.h
@@ -0,0 +1,19 @@
+/*
+ * clock_pwr.h
+ *
+ *  Created on: Jan 7, 2024
+ *      Author: Pavithraa
+ */
+
+#ifndef INCLUDE_CORE_LAYER_CLOCK_PWR_H_
+#define INCLUDE_CORE_LAYER_CLOCK_PWR_H_
+
+/* Includes for definitions */
+#include "common_header.h"
+#include "main.h"
+/* Public Variable Definitions */
+
+/* Public Function Prototypes **/
+extern U8 clock_pwr_system_clock_config_u8(void);
+
+#endif /* INCLUDE_CORE_LAYER_CLOCK_PWR_H_ */
